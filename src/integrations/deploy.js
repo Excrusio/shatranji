bc808925d2ba617bf11faf3190b581aaf798fe4d
Prakhar <47177028+Excrusio@users.noreply.github.com>
2022-05-05 11:12:19 +0530

    removed config in HDwallet

--- src/integrations/deploy.js
+++ src/integrations/deploy.js
@@ -2,10 +2,9 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
-const provider = new HDWalletProvider(
-	"tool season plug stadium chronic minute tattoo reunion tonight napkin ticket butter",
-	"https://rinkeby.infura.io/v3/d4ddb7c2d5c0437782581a4f952f9cc5"
-);
+const provider = new HDWalletProvider();
+// "tool season plug stadium chronic minute tattoo reunion tonight napkin ticket butter",
+// "https://rinkeby.infura.io/v3/d4ddb7c2d5c0437782581a4f952f9cc5"
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -13,7 +12,9 @@ const deploy = async () => {
 
 	console.log("Attempting to deploy from account", accounts[0]);
 
-	const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({ data: bytecode }).send({ gas: "1000000", from: accounts[0] });
+	const result = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode })
+		.send({ gas: "1000000", from: accounts[0] });
 
 	console.log("Contract deployed to", result.options.address);
 	provider.engine.stop();
